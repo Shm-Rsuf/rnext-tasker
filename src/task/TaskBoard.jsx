c8004b989ff17030af660d77671d43bec313a2e2
shm rsuf <shmusuf@gmail.com>
2024-01-17 07:21:11 +0600

    add task feature done

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -15,16 +16,25 @@ export default function TaskBoard() {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  /* handleAddTask */
+  const handleAddTask = (task) => {
+    setTasks([...tasks, task]);
+    setShowAddModal(false);
+    console.log(task);
+  };
 
   return (
     <section className='mb-20' id='tasks'>
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className='container'>
         <div className='p-2 flex justify-end'>
           <SearchTask />
         </div>
 
         <div className='rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16'>
-          <TaskActions />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
